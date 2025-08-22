04f90643473ab725be519f450b7094f0885ce28d
muriki rakesh <rakeshm@oipinsurtech.com>
2025-08-22 23:56:18 +0530

    Data Structure & Algorithm Challenges

--- dsa_challenges.py
+++ dsa_challenges.py
@@ -2,7 +2,7 @@
 def filter_and_sort_evens(numbers):
     evens = [num for num in numbers if num % 2 == 0]
     return sorted(evens)
-print(filter_and_sort_evens([3, 1, 4, 1, 5, 9, 2, 6]))
+print(filter_and_sort_evens([5,8,1,2,4,6,9,3]))
 
 #count_character_frequency(text)
 def count_character_frequency(text):
